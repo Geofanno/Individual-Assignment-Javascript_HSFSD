4b24b74e7beee56c89ec686f942095a150ce4fa5
geofanno <geofanno710@gmail.com>
2023-10-19 19:48:36 +0800

    Tugas 2

---
+++ question.js
@@ -0,0 +1,61 @@
+let questions = [{
+        numb: 1,
+        question: "Apa singkatan dari HTML?",
+        answer: "A. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Markup Language",
+            "B. High-level Text Markup Language",
+            "C. Hyperlink and Text Markup Language",
+            "D. Hyper Transfer Markup Language",
+            "E. Semua salah"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Mana yang bukan jenis elemen HTML?",
+        answer: "C. format",
+        options: [
+            "A. div",
+            "B. section",
+            "C. format",
+            "D. p",
+            "E. header"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Bagaimana cara menggabungkan CSS eksternal ke dalam halaman HTML?",
+        answer: "B. Menggunakan tag link dengan atribut href yang mengarah ke file CSS",
+        options: [
+            "A. Menggunakan tag  style di dalam elemen HTM",
+            "B. Menggunakan tag link dengan atribut href yang mengarah ke file CSS",
+            "C. Menulis CSS langsung di dalam elemen HTML",
+            "D. Menggunakan tag script dengan atribut src yang mengarah ke file CSS",
+            "E. Tidak mungkin menggabungkan CSS eksternal ke dalam HTML"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Apa peran JavaScript dalam pengembangan web?",
+        answer: "D. Membuat interaktivitas dan fungsi di halaman web",
+        options: [
+            "A. Menyusun tata letak halaman web",
+            "B. Menerapkan gaya dan desain ke halaman web",
+            "C. Mengatur tampilan halaman web",
+            "D. Membuat interaktivitas dan fungsi di halaman web",
+            "E. Membuat dokumen teks HTML"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Bagaimana Anda menambahkan komentar dalam kode JavaScript?",
+        answer: "D. Menggunakan tanda dua garis miring (//) atau tanda / / untuk komentar multiline",
+        options: [
+            "A. Menggunakan tanda pagar (#)",
+            "B. Menggunakan tanda seru (!)",
+            "C. Menggunakan tanda asterisk () di awal dan akhir komentar",
+            "D. Menggunakan tanda dua garis miring (//) atau tanda / / untuk komentar multiline",
+            "E. JavaScript tidak mendukung komentar"
+        ]
+    },
+]
